be5e216209c1c4471f0c934a2423d7b09baacfa3
Cunningham <pcunningham@tridium.com>
2018-01-31 14:59:11 -0500

    it should work now

--- src/SpecialWebDetectFetcher.java
+++ src/SpecialWebDetectFetcher.java
@@ -2,26 +2,30 @@ import net.azib.ipscan.config.ScannerConfig;
 import net.azib.ipscan.fetchers.PortTextFetcher;
 
 /**
- * Created by Patrick Cunningham on 5/5/2017.
+ * Created by p.elsie on 5/5/2017.
  */
 
 
 public class SpecialWebDetectFetcher extends PortTextFetcher {
 	public SpecialWebDetectFetcher() {
-		this(null);
+		this(Config.getConfig().forScanner());
 	}
 
 	public SpecialWebDetectFetcher(ScannerConfig scannerConfig) {
-		super(scannerConfig, 80, "HEAD /robots.txt HTTP/1.0\r\n\r\n", "^[Ss]erver:\\s+(.*)$");
+		super(scannerConfig, 8080, "HEAD /robots.txt HTTP/1.0\r\n\r\n", "^[Ss]erver:\\s+(.*)$");
 	}
 
 	public SpecialWebDetectFetcher(ScannerConfig scannerConfig, int defaultPort, String textToSend, String matchingRegexp) {
 		super(scannerConfig, defaultPort, textToSend, matchingRegexp);
 	}
 
+	@Override
+	public String getName() {
+		return "Special Web Detect - reads HTTP headers on non-standard port";
+	}
 
 	@Override
 	public String getId() {
-		return "fetcher.webDetect";
+		return "fetcher.specialWebDetect";
 	}
 }
